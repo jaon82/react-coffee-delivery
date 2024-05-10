6d26a7821c259abbfe41d02a5d80e3a4242df2ec
Jean Nunes <jean.nunes@uzzipay.com>
2024-05-10 19:42:46 -0400

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
+import Router from "./Router";
 import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
