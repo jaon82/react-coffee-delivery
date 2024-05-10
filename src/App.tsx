d9ca42d5c227585846e583e035470de4ac678057
Jean Nunes <jean.nunes@uzzipay.com>
2024-05-10 19:15:48 -0400

    Create theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 function App() {
-  return <>Coffee Delivery</>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
