edc4899178f8c18df3805cb9d273bf270002d96f
Jean Nunes <jean.nunes@uzzipay.com>
2024-05-17 18:22:42 -0400

    OrderInfo border

--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -22,15 +22,34 @@ export const OrderInfoContent = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  gap: 6.375rem;
 `;
 
 export const OrderInfo = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   gap: 2rem;
   padding: 2.5rem;
-  border: solid 1px ${(props) => props.theme.yellow};
+  flex: 1;
+
+  &::before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    padding: 1px;
+    border-radius: 8px 32px;
+    background: linear-gradient(
+      45deg,
+      ${(props) => props.theme.yellow},
+      ${(props) => props.theme.purple}
+    );
+    -webkit-mask: linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+  }
 
   > div {
     display: flex;
